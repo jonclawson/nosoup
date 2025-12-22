05bd035a7473da39b6465015359623d7b1513e2b
jonclawson <30538332+jonclawson@users.noreply.github.com>
2025-12-21 18:37:17 -0800

    21600 db retension

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,6 +44,7 @@ resource "neon_project" "nosoup_db" {
   region_id          = var.neon_region
   pg_version         = 16
   compute_provisioner = "k8s-pod"
+  requested_history_retention_seconds = 21600  # max 6 hours for free tier
 
   lifecycle {
     prevent_destroy = false  # Set to true in production to prevent accidental deletion
