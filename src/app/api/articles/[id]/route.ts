f4d2552a64015aa2d6893783f7359620ad9c2791
jonclawson <30538332+jonclawson@users.noreply.github.com>
2025-12-22 23:13:01 -0800

    debug R2 uploads

--- src/app/api/articles/[id]/route.ts
+++ src/app/api/articles/[id]/route.ts
@@ -126,6 +126,7 @@ export async function PUT(
             };
 
             try {
+              console.log(`Uploading ${key} to R2 bucket ${bucketName}... ${process.env.R2_ACCOUNT_ID} -- ${process.env.R2_ACCESS_KEY_ID} -- ${process.env.R2_SECRET_ACCESS_KEY}`);
               await s3Client.send(new PutObjectCommand(putObjectParams));
               console.log(`Successfully uploaded ${key} to R2 bucket ${bucketName}`);
               field.value = `https://${bucketName}.${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com/${key}`;
