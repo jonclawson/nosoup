5fc53f9b21b9198ab2394c08b7248ee58a5bba9f
jonclawson <30538332+jonclawson@users.noreply.github.com>
2025-12-23 23:52:42 -0800

    cf v4

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
     neon = {
       source  = "kislerdm/neon"
