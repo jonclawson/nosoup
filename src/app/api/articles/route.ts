5cc54ea8c9ab6751c53b068b7eba42769a492be8
jonclawson <30538332+jonclawson@users.noreply.github.com>
2025-12-25 11:53:21 -0800

    fix article list options

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -31,9 +31,9 @@ export async function GET(request: NextRequest) {
       prisma.article.findMany({
         where: {
           AND: [
-            featured != null ? {featured: true} : {},
+            featured === 'true' ? {featured: true} : {},
             {
-              OR: published != null ? [
+              OR: published === 'true' ? [
                  {published: true},
                 { 
                   AND: [
@@ -69,7 +69,7 @@ export async function GET(request: NextRequest) {
           },
         },
         orderBy: [
-          sticky != null ? { sticky: 'desc' } : {},
+          sticky === 'true' ? { sticky: 'desc' } : {},
           { createdAt: 'desc' },
         ],
         skip,
