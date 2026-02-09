217bcfe8b96778b6b6ac60ead7a8b9903f510785
Jonathan Clawson <jonathanclawson@Jonathans-MacBook-Pro.local>
2026-02-09 11:15:03 -0800

    allow speech in iframe

--- src/components/CodeField.tsx
+++ src/components/CodeField.tsx
@@ -5,7 +5,9 @@ export default function CodeField({ article, field }: { article: any, field: any
   return <>
           {article && article?.author?.role === 'admin' ? (
             // <div dangerouslySetInnerHTML={{ __html: field.value }} />
-            <iframe src={`/api/code/${field.id}`} className={styles['article-fields__code-iframe']} />
+            <iframe src={`/api/code/${field.id}`} 
+            allow="microphone *; speech-recognition *; on-device-speech-recognition *"
+            className={styles['article-fields__code-iframe']} />
             ) : (
               <Dompurify html={field.value } />
             )}
