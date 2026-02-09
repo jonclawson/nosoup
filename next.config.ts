e4a986cd1aa56bc18b9f5f8c5ba578109b3b1cbb
Jonathan Clawson <jonathanclawson@Jonathans-MacBook-Pro.local>
2026-02-09 11:19:31 -0800

    enable csp

--- next.config.ts
+++ next.config.ts
@@ -15,26 +15,26 @@ const csp = [
 ].join('; ');
 
 const nextConfig: NextConfig = {
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/(.*)",
-  //       headers: [
-  //         {
-  //           key: "Content-Security-Policy",
-  //           value: csp,
-  //         },
-  //         { key: "X-Content-Type-Options", value: "nosniff" },
-  //         // { key: "X-Frame-Options", value: "SAMEORIGIN" },
-  //         { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-  //         {
-  //           key: "Permissions-Policy",
-  //           value: "geolocation=(), microphone=(), camera=()",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: csp,
+          },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          // { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          // {
+          //   key: "Permissions-Policy",
+          //   value: "geolocation=(), microphone=(), camera=()",
+          // },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
